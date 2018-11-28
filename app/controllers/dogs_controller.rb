4c9fba64d2277f87270915917d516192481f3516
leannemcabey <leanne.m.cabey@gmail.com>
2018-11-28 11:25:02 -0500

    complete

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,11 @@
 class DogsController < ApplicationController
+
+  def index
+    @sorted_dogs = Dog.sorted_dogs
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,11 @@
 class Dog < ApplicationRecord
+  has_many :employees
+
+  def employee_count
+    employees.count
+  end
+
+  def self.sorted_dogs
+    Dog.all.sort_by {|dog| dog.employee_count}
+  end
 end
